68ede93e48c1835c97d504b3b73f7c83b27546a3
Vivek Singh <viveksingh.lp@gmail.com>
2019-04-13 09:41:00 +0530

    Create Contacts component

---
+++ src/components/Contacts.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Contact from './Contact';
+
+class Contacts extends Component {
+    constructor() {
+        super();
+        this.state = {
+            contacts: [
+                {
+                    id: 1,
+                    name: 'Chester Bennington',
+                    email: 'chesterbe@gmail.com',
+                    phone: '333-333-3333'
+                },
+                {
+                    id: 2,
+                    name: 'Brad Delson',
+                    email: 'bdelson@gmail.com',
+                    phone: '111-111-1111'
+                },
+                {
+                    id: 3,
+                    name: 'Rob Bourdon',
+                    email: 'robbourdon@gmail.com',
+                    phone: '222-222-2222'
+                }
+            ]
+        };
+    }
+
+    render() {
+        const { contacts } = this.state;
+        return (
+            <div>
+                {contacts.map(contact => (
+                    <Contact
+                        key={contact.id}
+                        name={contact.name}
+                        email={contact.email}
+                        phone={contact.phone}
+                    />
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Contacts;
