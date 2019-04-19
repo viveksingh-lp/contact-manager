c659713b89c8f075d3d34906b0fef210c9dbb62c
Vivek Singh <viveksingh.lp@gmail.com>
2019-04-19 22:05:29 +0530

    Add show-hide contact information toggler

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,24 +2,42 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+    state = {
+        showContactInfo: false
+    };
+
+    onShowClick = e => {
+        this.setState({
+            showContactInfo: !this.state.showContactInfo
+        });
+    };
+
     render() {
-        const { name, email, phone } = this.props;
+        const { name, email, phone } = this.props.contact;
+        const { showContactInfo } = this.state;
         return (
             <div className="card card-body mb-3">
-                <h4>{name}</h4>
-                <ul className="list-group">
-                    <li className="list-group-item">Email: {email}</li>
-                    <li className="list-group-item">Phone: {phone}</li>
-                </ul>
+                <h4>
+                    {name}{' '}
+                    <i
+                        onClick={this.onShowClick}
+                        className="fas fa-sort-down"
+                        style={{ cursor: 'pointer' }}
+                    />
+                </h4>
+                {showContactInfo ? (
+                    <ul className="list-group">
+                        <li className="list-group-item">Email: {email}</li>
+                        <li className="list-group-item">Phone: {phone}</li>
+                    </ul>
+                ) : null}
             </div>
         );
     }
 }
 
 Contact.propTypes = {
-    name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    phone: PropTypes.string.isRequired
+    contact: PropTypes.object.isRequired
 };
 
 export default Contact;
