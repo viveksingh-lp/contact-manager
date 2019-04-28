efc9db053229c7711559b5203c76e31d53ae3ff5
Vivek Singh <viveksingh.lp@gmail.com>
2019-04-28 14:44:39 +0530

    Implement Add functionality using Reducer

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Consumer } from './../../Context';
+import uuid from 'uuid';
 
 class AddContact extends Component {
     state = {
@@ -13,61 +15,86 @@ class AddContact extends Component {
         });
     };
 
-    onSubmit = e => {
+    onSubmit = (dispatch, e) => {
         e.preventDefault();
-        console.log(this.state);
+        const { name, email, phone } = this.state;
+        const newContact = {
+            id: uuid(),
+            name,
+            email,
+            phone
+        };
+        dispatch({ type: 'ADD_CONTACT', payload: newContact });
+
+        this.setState({
+            name: '',
+            email: '',
+            phone: ''
+        });
     };
 
     render() {
         const { name, email, phone } = this.state;
         return (
-            <div>
-                <div className="card mb-3">
-                    <div className="card-header">Add Contact</div>
-                    <div className="card-body">
-                        <form onSubmit={this.onSubmit}>
-                            <div className="form-group">
-                                <label htmlFor="name">Name</label>
-                                <input
-                                    type="text"
-                                    name="name"
-                                    id="name"
-                                    className="form-control"
-                                    value={name}
-                                    onChange={this.onChange}
-                                />
+            <Consumer>
+                {value => {
+                    const { dispatch } = value;
+                    return (
+                        <div>
+                            <div className="card mb-3">
+                                <div className="card-header">Add Contact</div>
+                                <div className="card-body">
+                                    <form
+                                        onSubmit={this.onSubmit.bind(
+                                            this,
+                                            dispatch
+                                        )}
+                                    >
+                                        <div className="form-group">
+                                            <label htmlFor="name">Name</label>
+                                            <input
+                                                type="text"
+                                                name="name"
+                                                id="name"
+                                                className="form-control"
+                                                value={name}
+                                                onChange={this.onChange}
+                                            />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="email">Email</label>
+                                            <input
+                                                type="email"
+                                                name="email"
+                                                id="email"
+                                                className="form-control"
+                                                value={email}
+                                                onChange={this.onChange}
+                                            />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="phone">Phone</label>
+                                            <input
+                                                type="text"
+                                                name="phone"
+                                                id="phone"
+                                                className="form-control"
+                                                value={phone}
+                                                onChange={this.onChange}
+                                            />
+                                        </div>
+                                        <input
+                                            type="submit"
+                                            className="btn btn-secondary btn-block"
+                                            value="Add Contact"
+                                        />
+                                    </form>
+                                </div>
                             </div>
-                            <div className="form-group">
-                                <label htmlFor="email">Email</label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    className="form-control"
-                                    value={email}
-                                    onChange={this.onChange}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="phone">Phone</label>
-                                <input
-                                    type="text"
-                                    name="phone"
-                                    id="phone"
-                                    className="form-control"
-                                    value={phone}
-                                    onChange={this.onChange}
-                                />
-                            </div>
-                            <input
-                                type="submit"
-                                className="btn btn-secondary btn-block"
-                                value="Add Contact"
-                            />
-                        </form>
-                    </div>
-                </div>
-            </div>
+                        </div>
+                    );
+                }}
+            </Consumer>
         );
     }
 }
