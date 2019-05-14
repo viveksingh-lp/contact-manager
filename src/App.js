4926f2231b5b58819968729d0185c13bf17a59ab
Vivek Singh <viveksingh.lp@gmail.com>
2019-05-14 19:04:25 +0530

    Add Edit Contact component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
 import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 import { Provider } from './Context';
@@ -25,6 +26,11 @@ class App extends Component {
                                     exact
                                     path="/contact/add"
                                     component={AddContact}
+                                />
+                                <Route
+                                    exact
+                                    path="/contact/edit/:id"
+                                    component={EditContact}
                                 />
                                 <Route component={NotFound} />
                             </Switch>
--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import { Consumer } from '../../Context';
@@ -53,6 +54,17 @@ class Contact extends Component {
                                         id
                                     )}
                                 />
+                                <Link to={`contact/edit/${id}`}>
+                                    <i
+                                        className="fas fa-pencil-alt"
+                                        style={{
+                                            color: 'black',
+                                            float: 'right',
+                                            cursor: 'pointer',
+                                            marginRight: '1rem'
+                                        }}
+                                    />
+                                </Link>
                             </h4>
                             {showContactInfo ? (
                                 <ul className="list-group">
