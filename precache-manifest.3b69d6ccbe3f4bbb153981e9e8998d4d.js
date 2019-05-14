ad89c2d0edc2e18f573438879cfb71ff3495eb88
Vivek Singh <viveksingh.lp@gmail.com>
2019-05-14 19:55:23 +0530

    Updates

---
+++ precache-manifest.3b69d6ccbe3f4bbb153981e9e8998d4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e857deb823a1df66a7d6",
+    "url": "/contact-manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "e857deb823a1df66a7d6",
+    "url": "/contact-manager/static/js/main.a13c9ded.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  },
+  {
+    "revision": "14068ec525c13438481c",
+    "url": "/contact-manager/static/css/2.c062e498.chunk.css"
+  },
+  {
+    "revision": "14068ec525c13438481c",
+    "url": "/contact-manager/static/js/2.db983b87.chunk.js"
+  },
+  {
+    "revision": "fa9aaf8b704a8333b903b501c00c4685",
+    "url": "/contact-manager/index.html"
+  }
+];
